4db50c125d1ba7abe3cb5ca583d0dd561733cb72
liwenbo <liwenbo@supermonkey.com.cn>
2023-01-30 11:49:41 +0800

    fix: update example.

--- example/lib/example_route.dart
+++ example/lib/example_route.dart
@@ -5,6 +5,7 @@
 
 import 'package:ff_annotation_route_library/ff_annotation_route_library.dart';
 import 'package:flutter/widgets.dart';
+
 import 'pages/complex/dou_yin_ping_lun.dart';
 import 'pages/complex/load_more.dart';
 import 'pages/complex/pull_to_refresh.dart';
@@ -18,15 +19,15 @@ import 'pages/simple/extened_nested_scroll_view_demo.dart';
 FFRouteSettings getRouteSettings({
   @required String name,
   Map<String, dynamic> arguments,
+  PageBuilder notFoundPageBuilder,
 }) {
-  final Map<String, dynamic> safeArguments =
-      arguments ?? const <String, dynamic>{};
+  final Map<String, dynamic> safeArguments = arguments ?? const <String, dynamic>{};
   switch (name) {
     case 'fluttercandies://PingLunDemo':
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: PingLunDemo(),
+        builder: () => PingLunDemo(),
         routeName: 'pingLundemo',
         pageRouteType: PageRouteType.transparent,
         description: 'Tik Tok Comment',
@@ -35,7 +36,7 @@ FFRouteSettings getRouteSettings({
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: TextFieldPage(
+        builder: () => TextFieldPage(
           text: asT<String>(safeArguments['text']),
         ),
         routeName: 'TextFieldPage',
@@ -46,7 +47,7 @@ FFRouteSettings getRouteSettings({
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: DouYinPingLunDemo(),
+        builder: () => DouYinPingLunDemo(),
         routeName: 'tiktokcomment',
         description: 'Demo for Tik Tok Comment',
         exts: <String, dynamic>{'group': 'Complex', 'order': 4},
@@ -55,9 +56,8 @@ FFRouteSettings getRouteSettings({
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: DemoGroupPage(
-          keyValue: asT<MapEntry<String, List<DemoRouteResult>>>(
-              safeArguments['keyValue']),
+        builder: () => DemoGroupPage(
+          keyValue: asT<MapEntry<String, List<DemoRouteResult>>>(safeArguments['keyValue']),
         ),
         routeName: 'DemoGroupPage',
       );
@@ -65,24 +65,23 @@ FFRouteSettings getRouteSettings({
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: LoadMoreDemo(),
+        builder: () => LoadMoreDemo(),
         routeName: 'load more demo',
-        description:
-            "show how to support load more list in NestedScrollView's body without ScrollController",
+        description: "show how to support load more list in NestedScrollView's body without ScrollController",
         exts: <String, dynamic>{'group': 'Complex', 'order': 2},
       );
     case 'fluttercandies://mainpage':
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: MainPage(),
+        builder: () => MainPage(),
         routeName: 'MainPage',
       );
     case 'fluttercandies://nestedscrollview':
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: OldExtendedNestedScrollViewDemo(),
+        builder: () => OldExtendedNestedScrollViewDemo(),
         routeName: 'NestedScrollview',
         description: 'fix pinned header and inner scrollables sync issues.',
         exts: <String, dynamic>{'group': 'Simple', 'order': 0},
@@ -91,7 +90,7 @@ FFRouteSettings getRouteSettings({
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: DynamicPinnedHeaderHeightDemo(),
+        builder: () => DynamicPinnedHeaderHeightDemo(),
         routeName: 'pinned header height',
         description: 'how to change pinned header height dynamically',
         exts: <String, dynamic>{'group': 'Simple', 'order': 1},
@@ -100,30 +99,27 @@ FFRouteSettings getRouteSettings({
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: PullToRefreshDemo(),
+        builder: () => PullToRefreshDemo(),
         routeName: 'pull to refresh',
-        description:
-            "how to pull to refresh for list in NestedScrollView's body without ScrollController",
+        description: "how to pull to refresh for list in NestedScrollView's body without ScrollController",
         exts: <String, dynamic>{'group': 'Complex', 'order': 0},
       );
     case 'fluttercandies://pulltorefreshouter':
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: PullToRefreshOuterDemo(),
+        builder: () => PullToRefreshOuterDemo(),
         routeName: 'pull to refresh outer',
-        description:
-            'how to pull to refresh for NestedScrollView without ScrollController',
+        description: 'how to pull to refresh for NestedScrollView without ScrollController',
         exts: <String, dynamic>{'group': 'Complex', 'order': 1},
       );
     case 'fluttercandies://scroll to top':
       return FFRouteSettings(
         name: name,
         arguments: arguments,
-        widget: ScrollToTopDemo(),
+        builder: () => ScrollToTopDemo(),
         routeName: 'scroll to top',
-        description:
-            "how to scroll list to top in NestedScrollView's body without ScrollController",
+        description: "how to scroll list to top in NestedScrollView's body without ScrollController",
         exts: <String, dynamic>{'group': 'Complex', 'order': 3},
       );
     default:
