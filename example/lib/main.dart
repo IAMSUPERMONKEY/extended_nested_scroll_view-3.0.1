aa57e9963382a1bb4b8309667fabda623fd6ce91
liwenbo <liwenbo@supermonkey.com.cn>
2023-01-30 11:15:04 +0800

    fix: fork 3.0.1

---
+++ example/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:ff_annotation_route_library/ff_annotation_route_library.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'example_route.dart';
+import 'example_routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'extended_nested_scroll_view demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: Routes.fluttercandiesMainpage,
+      onGenerateRoute: (RouteSettings settings) {
+        return onGenerateRoute(
+          settings: settings,
+          getRouteSettings: getRouteSettings,
+          // routeSettingsWrapper: (FFRouteSettings ffRouteSettings) {
+          //   if (ffRouteSettings.name == Routes.fluttercandiesMainpage ||
+          //       ffRouteSettings.name == Routes.fluttercandiesDemogrouppage) {
+          //     return ffRouteSettings;
+          //   }
+          //   return ffRouteSettings.copyWith(
+          //       widget: CommonWidget(
+          //     child: ffRouteSettings.widget,
+          //     title: ffRouteSettings.routeName,
+          //   ));
+          // },
+        );
+      },
+    );
+  }
+}
